b91fbbca2373bdfc50a6f97658dfcd8d49d8feff
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-02-13 00:39:47 +0530

    BASIC Programs on java DSA

--- src/Arrays/rotateArray.java
+++ src/Arrays/rotateArray.java
@@ -9,13 +9,11 @@ public class rotateArray {
 
         for(int i = 0 ; i < r ; i++){
             int temp = arr[n-1];
-
             for(int j = n-1 ; j > 0 ; j--){
                 arr[j] = arr[j-1];
             }
             arr[0] = temp;
         }
-
         for (int ele : arr) System.out.print(ele + " ");
     }
 }
