5dc47277548c09a8149773c8ff4f1948253de11d
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-02-13 18:05:21 +0530

    BASIC Programs on java DSA

--- src/Arrays/rotateArray.java
+++ src/Arrays/rotateArray.java
@@ -1,19 +1,30 @@
 package Arrays;
 
 public class rotateArray {
+
+    public static void reverse(int[] arr , int i , int j){
+
+        // LEFT SHIFTING (Anti-clockWise)
+
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         int n = arr.length;
-        int r = 2;
+        int r = 3;
+        r%=n;
 
-        for(int i = 0 ; i < r ; i++){
-            int temp = arr[n-1];
-            for(int j = n-1 ; j > 0 ; j--){
-                arr[j] = arr[j-1];
-            }
-            arr[0] = temp;
-        }
+        reverse(arr,0,r-1);
+        reverse(arr,r,n-1);
+        reverse(arr,0,n-1);
+
         for (int ele : arr) System.out.print(ele + " ");
     }
 }
