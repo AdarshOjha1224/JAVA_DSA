0bae2b899e121d5f1271614c8831ec7579dd1f6b
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-04-03 20:16:33 +0530

    BASIC Programs on java DSA

--- src/Pattern_Printing/AlphabetSquare.java
+++ src/Pattern_Printing/AlphabetSquare.java
@@ -10,7 +10,34 @@ public class AlphabetSquare {
 
         for (int i = 1 ; i <= n ; i++){
             for (int j = 1 ; j <= n ; j++){
-                System.out.print((char)(64+j) + " ");
+                System.out.print((char)(64+j) + "  ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= n ; j++){
+                System.out.print((char)(96+j) + "  ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= n ; j++){
+                System.out.print((char)(64+i) + "  ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= n ; j++){
+                System.out.print((char)(96+i) + "  ");
             }
             System.out.println();
         }
