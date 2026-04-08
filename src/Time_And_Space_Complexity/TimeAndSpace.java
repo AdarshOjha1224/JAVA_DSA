c7328a6d87b7e00f0be97da1e1ae14c582512cf3
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-04-08 22:37:28 +0530

    Time and Space Complexity Important Notes.

---
+++ src/Time_And_Space_Complexity/TimeAndSpace.java
@@ -0,0 +1,17 @@
+package Time_And_Space_Complexity;
+
+public class TimeAndSpace {
+//    Faster code is proportional to Number of lines.
+//    If number of Operation is n then time complexity is O(n).
+//    O(k.n) = O(n)
+//    O(n +|- k) = O(n)
+//    O(constant) = O(1)
+
+//    Space Complexity - Total Space taken to solve the problem
+//    Auxiliary Space - Extra Space taken to solve the problem
+
+//    Which one is fast ?
+//    1 < log n < n < n.log n < n^2 < n^3 <<< 2^n
+//    0(1) > 0(log n) > 0(n^1/2) > 0(n) > 0(n.log n) > 0(n^2) > 0(n^3) >> 0(2^n)
+
+}
