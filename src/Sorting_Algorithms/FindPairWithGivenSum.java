9416f0d72dbb44d359105da175c500a7c26857cf
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-04-11 23:21:39 +0530

    Finding the pairs of element in the array whose sum is equal to the given target.

---
+++ src/Sorting_Algorithms/FindPairWithGivenSum.java
@@ -0,0 +1,27 @@
+package Sorting_Algorithms;
+import java.util.Arrays;
+
+// Sorted array is given , finding pairs of number which is equal to target.
+
+public class FindPairWithGivenSum {
+    public static void pair(int[] arr , int target){
+        int n  = arr.length;
+        int i =  0;
+        int j = n-1;
+        while(i<j) {
+            if (arr[i] + arr[j] == target) {
+                System.out.println("(" + arr[i] + "," + arr[j] + ")");
+                i++;
+                j--;
+            }
+            else if (arr[i] + arr[j] > target) j--;
+            else if (arr[i] + arr[j] < target) i++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,4,45,12,6,5,10,11,8};
+        int target = 16;
+        Arrays.sort(arr);
+        pair(arr,target);
+    }
+}
