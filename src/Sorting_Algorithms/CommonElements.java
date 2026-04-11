2f8e3638ef7fdeb3ab4b9d03190fd562fea77904
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-04-11 23:22:20 +0530

    Finding common elements in two arrays and return the value in form of ArrayList.

---
+++ src/Sorting_Algorithms/CommonElements.java
@@ -0,0 +1,29 @@
+package Sorting_Algorithms;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CommonElements {
+    public static ArrayList<Integer> commEle(int[] a, int[] b) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int i = 0 , j = 0 ;
+        while(i < a.length && j < b.length) {
+            if(a[i] == b[j]) {
+                arr.add(a[i]);
+                i++;
+                j++;
+            }
+            else if(a[i] > b[j]) j++;
+            else i++;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {1,3,2,2,7};
+        int[] b = {2,4,2,3,8,6,9};
+        System.out.println(commEle(a,b));
+    }
+}
