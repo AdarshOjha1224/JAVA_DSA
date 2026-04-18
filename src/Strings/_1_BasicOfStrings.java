a10db0d0ca505bd6d2132ec8715110513a7c99a6
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-04-18 23:52:19 +0530

    Basic Knowledge of String in DSA in java.

--- src/Strings/_1_BasicOfStrings.java
+++ src/Strings/_1_BasicOfStrings.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class _1_BasicOfStrings {
     public static void main(String[] args) {
         // String is a Character Array.
+
         char[] ch = {'a','d','a','r','s','h'};
         for(char ele : ch) System.out.print(ele);
         String str = "Adarsh";
         System.out.println(str);
+
+        // Difference between next() & nextLine() ->>
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a string : ");
         String str1 = input.next();
