d66ee559b143606b3671c4bd374c0957a2a8f527
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-06-24 20:43:56 +0530

    Taking input from user.

--- src/Basics/TakingInput.java
+++ src/Basics/TakingInput.java
@@ -14,7 +14,9 @@ public class TakingInput {
 
         System.out.print("Enter the value of radius: ");
         double R = sc.nextDouble();
+
         double V = 4.0/3 * 3.141592 * R * R * R;
+
         System.out.println("The volume is " + V);
     }
 }
