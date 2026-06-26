704ddd8877175c0a354514d2b30bfe0ccdf07202
AdarshOjha1224 <adarshojha1224@gmail.com>
2026-06-26 23:45:09 +0530

    Checking if the given array data is sorted or not.

--- src/Sorting_Algorithms/_1_CheckSorted.java
+++ src/Sorting_Algorithms/_1_CheckSorted.java
@@ -3,9 +3,11 @@ package Sorting_Algorithms;
 public class _1_CheckSorted {
 
     public static boolean isSorted(int[] arr) {
+
         for(int i = 0 ; i < arr.length -1 ; i++){
             if(arr[i]>arr[i+1]) return false ;
         }
+
         return true;
     }
 
