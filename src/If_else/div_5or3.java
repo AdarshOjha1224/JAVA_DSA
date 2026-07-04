8de52a1c8e73216c5b271f2e8c77505f9c26abd7
adarsh <adarshojha1224@gmail.com>
2026-07-04 23:58:59 +0530

    simple if -else implemantaion.

--- src/If_else/div_5or3.java
+++ src/If_else/div_5or3.java
@@ -8,15 +8,11 @@ public class div_5or3 {
         System.out.print("Enter the value :");
         int n = sc.nextInt();
 
-
-
 //        if(n%3==0 && n%5!=0) System.out.println("Number is divisible by only 3");
 //        else if (n%5==0 && n%3!=0) System.out.println("Number is divisible by only 5");
 //        else if (n%5 == 0) System.out.println("Number is divisible by 3 & 5 both");
 //        else System.out.println("Number is not divisible by 3 & 5");
 //        smart way --->
-
-
 
         if(n%3==0 && n%5==0) System.out.println("Number is divisible by 3 & 5 both");
         else if (n%3==0) System.out.println("Number is divisible by 3");
