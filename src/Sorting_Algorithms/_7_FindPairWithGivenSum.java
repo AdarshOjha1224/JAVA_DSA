2fd7532fcf2f6a610ef4e6e7eb0e50d9554ec216
adarsh <adarshojha1224@gmail.com>
2026-07-05 20:25:23 +0530

    Sorted array is given , finding pairs of number which is equal to target.

--- src/Sorting_Algorithms/_7_FindPairWithGivenSum.java
+++ src/Sorting_Algorithms/_7_FindPairWithGivenSum.java
@@ -18,10 +18,12 @@ public class _7_FindPairWithGivenSum {
             else if (arr[i] + arr[j] < target) i++;
         }
     }
+
     public static void main(String[] args) {
         int[] arr = {1,4,45,12,6,5,10,11,8};
         int target = 16;
         Arrays.sort(arr);
         pair(arr,target);
+
     }
 }
